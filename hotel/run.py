8f18efe2cf8e31a98a4039522d7298b78aaef05e
uni4x <micro09188@gmail.com>
2024-06-01 21:35:54 +0900

    update run.py

--- hotel/run.py
+++ hotel/run.py
@@ -1,9 +1,15 @@
-# app/run.py
+# run.py
 import os
 from app import create_app
 
-config_name = os.getenv('FLASK_ENV', 'default')
-app = create_app(config_name)
+app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+
+# config_name = os.getenv('FLASK_ENV', 'default')
+# app = create_app(config_name)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
